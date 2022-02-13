d782d1a7ca3518da951e5beaaea293775bb17687
christopher <x10tion007@gmail.com>
2022-02-13 13:51:06 +0100

    function componant calls

---
+++ src/Componant/ClassComponant.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class ClassComponant extends React.Component() {
+    render () {
+    return  <p> these is a clas attribute </p>
+}
+}
---
+++ src/Componant/FuntionalComponant.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FunctionalComponant() {
+    return  <h1> these is an functional componant </h1>
+}
+
+export defualt FunctionalComponants;
